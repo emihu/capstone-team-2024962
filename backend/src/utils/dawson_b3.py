8745bc8d2bff9d5ca5098db33d06940f8f573596
andrewcleung <leung.andrew@hotmail.com>
2025-03-18 17:02:14 -0400

    fix angle problem

--- backend/src/utils/dawson_b3.py
+++ backend/src/utils/dawson_b3.py
@@ -18,12 +18,16 @@ def rad_to_deg(rad) -> float:
 def phi_to_lat(phi) -> float:
     """
     Convert the phi angle to latitude.
+    phi: The angle in radians.
+    return: The latitude in degrees.
     """
-    return rad_to_deg(90 - phi)
+    return rad_to_deg(math.pi / 2 - phi)
 
 def theta_to_lon(theta) -> float:
     """
     Convert the theta angle to longitude.
+    theta: The angle in radians.
+    return: The longitude in degrees.
     """
     return unconvert_longitude(rad_to_deg(theta))
 
