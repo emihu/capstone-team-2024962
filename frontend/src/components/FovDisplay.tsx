90186719081ea52b0c463b1080ea0f88e6bbd558
Emily <emihu891@gmail.com>
2025-03-20 21:08:51 -0400

    fix alignment for fov

--- frontend/src/components/FovDisplay.tsx
+++ frontend/src/components/FovDisplay.tsx
@@ -25,7 +25,7 @@ const FovDisplay: React.FC<FovDisplayProps> = ({
   remainingTimePercentage,
 }) => {
   return (
-    <Col className="mx-auto text-center">
+    <div className="container">
       <div className="fov-title">Field of View</div>
       <div className="fov-frame">
         {isLoading && (
@@ -47,40 +47,6 @@ const FovDisplay: React.FC<FovDisplayProps> = ({
               );
             }
             return null;
-            // return satelliteDataForSecond.map((satellite) => {
-            //   const { sid, delta_radius, delta_theta } = satellite;
-
-            //   let dotColor = "blue-dot";
-            //   if (index === 0) dotColor = "green-dot";
-            //   else if (index === satelliteDataForSecond.length - 2) dotColor = "red-dot";
-
-            //   if (index >= counter) {
-            //     // Calculate future position
-            //     const x = delta_radius * Math.sin(delta_theta * (Math.PI / 180));
-            //     const y = delta_radius * Math.cos(delta_theta * (Math.PI / 180));
-            //     const right = 0.5 - x / 2 - 0.07;
-            //     const top = 0.5 - y / 2 - 0.07;
-
-            //     const rightPercentage = `${right * 100 + 7}%`;
-            //     const topPercentage = `${top * 100 + 7}%`;
-
-            //     // Render red dot for future position only if the satellite is visible
-            //     if (visibleFlights.some((visibleFlights) => visibleFlights.sid === sid)) {
-            //       return (
-            //         <div
-            //           key={sid}
-            //           className={dotColor}
-            //           style={{
-            //             position: "absolute",
-            //             right: rightPercentage,
-            //             top: topPercentage,
-            //           }}
-            //         />
-            //       );
-            //     }
-            //   }
-            //   return null;
-            // });
           })}
         {/* Render currently visible satellites */}
         {!isLoading &&
@@ -94,19 +60,17 @@ const FovDisplay: React.FC<FovDisplayProps> = ({
       </div>
       {/* Exposure time pill */}
       {!isLoading && (
-        <div>
-          <Row>
-            <div className="pill-title">Next {currentExposureTime} seconds</div>
-          </Row>
-          <div className="col exposure-pill-container">
-            <div
-              className="exposure-pill"
-              style={{ width: `${remainingTimePercentage}%` }}
-            ></div>
-          </div>
+        <>
+        <div className="pill-title">Next {currentExposureTime} seconds</div>
+        <div className="exposure-pill-container">
+          <div
+            className="exposure-pill"
+            style={{ width: `${remainingTimePercentage}%` }}
+          ></div>
         </div>
+        </>
       )}
-    </Col>
+    </div>
   );
 };
 
