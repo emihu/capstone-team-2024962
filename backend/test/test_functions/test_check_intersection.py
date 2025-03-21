4906ba9a76c29dda843aea845f7993f4301bb6df
andrewcleung <leung.andrew@hotmail.com>
2025-03-20 23:59:57 -0400

    add some more test

--- backend/test/test_functions/test_check_intersection.py
+++ backend/test/test_functions/test_check_intersection.py
@@ -1,6 +1,21 @@
 import pytest
 import src.utils.dawson_d as dawson_d
 
+
+def generate_flight_coordinates(flight_start_ra, flight_start_dec, flight_end_ra, flight_end_dec, num_points):
+    flightRa = []
+    flightDec = []
+
+    # Generate flight coordinates using linear interpolation
+    for n in range(num_points):
+        t = n / (num_points - 1)  # normalized parameter from 0 to 1
+        ra = flight_start_ra + (flight_end_ra - flight_start_ra) * t
+        dec = flight_start_dec + (flight_end_dec - flight_start_dec) * t
+        flightRa.append(ra)
+        flightDec.append(dec)
+
+    return list(zip(flightRa, flightDec))
+
 @pytest.mark.parametrize(
     "fov_size, fov_ra, fov_dec, flight_start_ra, flight_start_dec, flight_end_ra, flight_end_dec, expected",
     [
@@ -20,18 +35,8 @@ import src.utils.dawson_d as dawson_d
 )
 def test_dawson_d2(fov_size, fov_ra, fov_dec, flight_start_ra, flight_start_dec, flight_end_ra, flight_end_dec, expected):
     num_points = 100
-    flightRa = []
-    flightDec = []
 
-    # Generate flight coordinates using linear interpolation
-    for n in range(num_points):
-        t = n / (num_points - 1)  # normalized parameter from 0 to 1
-        ra = flight_start_ra + (flight_end_ra - flight_start_ra) * t
-        dec = flight_start_dec + (flight_end_dec - flight_start_dec) * t
-        flightRa.append(ra)
-        flightDec.append(dec)
-
-    flightRaDec = list(zip(flightRa, flightDec))
+    flightRaDec = generate_flight_coordinates(flight_start_ra, flight_start_dec, flight_end_ra, flight_end_dec, num_points)
 
     ret = False
     for info in flightRaDec:
@@ -41,3 +46,27 @@ def test_dawson_d2(fov_size, fov_ra, fov_dec, flight_start_ra, flight_start_dec,
 
     assert ret == expected
 
+@pytest.mark.parametrize(
+    "fov_size, fov_ra, fov_dec, flight_start_ra, flight_start_dec, flight_end_ra, flight_end_dec",
+    [
+        # RA negative: RA should be within 0 to 360 degrees.
+        (4.77464829, 0, 5, -1, 0, 0, 5),
+        # RA too high: values above 360 are invalid.
+        (4.77464829, 0, 5, 0, 0, 361, 5),
+        # Dec too high: declination must be between -90 and 90 degrees.
+        (4.77464829, 0, 5, 0, 0, 0, 91),
+        # Dec too low: values below -90 are invalid.
+        (4.77464829, 0, 5, 0, -91, 0, 5)
+    ]
+)
+def test_dawson_d_out_of_bounds(fov_size, fov_ra, fov_dec,
+                                flight_start_ra, flight_start_dec,
+                                flight_end_ra, flight_end_dec):
+    num_points = 10
+
+    flightRaDec = generate_flight_coordinates(flight_start_ra, flight_start_dec, flight_end_ra, flight_end_dec, num_points)
+
+    # Expecting a ValueError to be raised if any coordinate is out-of-bound
+    with pytest.raises(ValueError):
+        for info in flightRaDec:
+            _ = dawson_d.is_intersecting(info[0], info[1], fov_ra, fov_dec, fov_size)
